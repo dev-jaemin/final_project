8bdabdf7f7fd3d9da50652400bd8f33ef748117f
zzamny1013 <msyh0207@naver.com>
2018-11-20 23:56:09 +0900

    Nov 20

--- src/BabyakPackage/list.java
+++ src/BabyakPackage/list.java
@@ -1,5 +1,9 @@
 package BabyakPackage;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class restaurant{
 	String name;
 	String location;
@@ -20,9 +24,25 @@ public class restaurant{
 
 //파일 입출력을 통해 메모장에 저장하는 기능 구현
 
-public class list {
+public class list extends JFrame {
 	//List GUI구현하기
-	
+	public list() {
+		setTitle("밥약해요!");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Container listcp = getContentPane();
+		public JPanel listpanel = new JPanel();
+		
+		JButton testbutton1 = new JButton("테스트용");
+		JLabel testlabel = new JLabel("테스트라벨");
+		
+		listpanel.add(testbutton1);
+		listpanel.add(testlabel);
+		//listcp.add(listpanel);
+		
+		//listcp.setSize(400,400);
+		//listcp.setVisible(true);
+	}
 	
 	
 	//그에 맞는 이벤트도 구현하기
