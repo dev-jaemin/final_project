e3cb6ccfd6f08c6ee98d667781042ab9b0a4ea23
zzamny1013 <msyh0207@naver.com>
2018-11-27 00:41:37 +0900

    Nov 26

--- src/BabyakPackage/main.java
+++ src/BabyakPackage/main.java
@@ -93,21 +93,72 @@ public class main extends JFrame{
 		
 		//recommand panel 코드
 		
-		//recommandpanel.add(testbtn11);
+		JPanel recommandpanel1 = new JPanel();
+		JLabel option_ =  new JLabel("<검색 조건>");
+		
+		JLabel priceLabel_ = new JLabel("가격대");
+		JCheckBox under5000_ = new JCheckBox("5000원 미만");
+		JCheckBox under7500_ = new JCheckBox("5000원 ~ 7500원");
+		JCheckBox under10000_ = new JCheckBox("7500원 ~ 10000원");
+		JCheckBox under15000_ = new JCheckBox("10000원 ~ 15000원");
+		JCheckBox over15000_ = new JCheckBox("15000원 이상");
+		recommandpanel1.add(priceLabel_);
+		recommandpanel1.add(under5000_);
+		recommandpanel1.add(under7500_);
+		recommandpanel1.add(under10000_);
+		recommandpanel1.add(under15000_);
+		recommandpanel1.add(over15000_);
+		
+		JPanel recommandpanel2 = new JPanel();
+		JLabel locationLabel_ = new JLabel("<위치>");
+		JCheckBox loc_A_ = new JCheckBox("정후");
+		JCheckBox loc_B_ = new JCheckBox("개운사길");
+		JCheckBox loc_C_ = new JCheckBox("참살이길");
+		JCheckBox loc_D_ = new JCheckBox("옆살이, 옆옆살이");
+		JCheckBox loc_E_ = new JCheckBox("법후");
+		JCheckBox loc_F_ = new JCheckBox("제기동");
+		JCheckBox loc_G_ = new JCheckBox("학교 안");
+		recommandpanel2.add(locationLabel_);
+		recommandpanel2.add(loc_A_);
+		recommandpanel2.add(loc_B_);
+		recommandpanel2.add(loc_C_);
+		recommandpanel2.add(loc_D_);
+		recommandpanel2.add(loc_E_);
+		recommandpanel2.add(loc_F_);
+		recommandpanel2.add(loc_G_);
+		
+		JPanel recommandpanel3 = new JPanel();
+		JLabel categoryLabel_ = new JLabel("<종류>");
+		JCheckBox cat_A_ = new JCheckBox("양식");
+		JCheckBox cat_B_ = new JCheckBox("한식");
+		JCheckBox cat_C_ = new JCheckBox("중식");
+		JCheckBox cat_D_ = new JCheckBox("일식");
+		JCheckBox cat_E_ = new JCheckBox("분식 & 간단한");
+		JCheckBox cat_F_ = new JCheckBox("기타 국가");
+		recommandpanel3.add(categoryLabel_);
+		recommandpanel3.add(cat_A_);
+		recommandpanel3.add(cat_B_);
+		recommandpanel3.add(cat_C_);
+		recommandpanel3.add(cat_D_);
+		recommandpanel3.add(cat_E_);
+		recommandpanel3.add(cat_F_);
+		
+		JList<String> resultRecommand = new JList<String>();
+		
+		recommandpanel.add(option_);
+		recommandpanel.add(recommandpanel1);
+		recommandpanel.add(recommandpanel2);
+		recommandpanel.add(recommandpanel3);
+		recommandpanel.add(resultRecommand);
 		
 		
 		//시작화면 기본 설정
 		contentPane.add(mainpanel);
-		setSize(400,400);
+		setSize(800,600);
 		setVisible(true);
 	}
 	
 	
-	//시작화면 이벤트 구현
-	
-	
-	
-
 	public static void main(String[] args) {
 		//실행 코드 구현
 		//메모장 파일이 없을 경우, 파일을 생성하는 기능 구현
--- src/BabyakPackage/restaurantClass.java
+++ src/BabyakPackage/restaurantClass.java
@@ -3,10 +3,12 @@ package BabyakPackage;
 public class restaurantClass {
 	String name;
 	String location;
+	String category;
 	int price;
 	
-	public restaurantClass(String name, String location, int price) {
+	public restaurantClass(String name, int price, String location, String category) {
 		this.name = name;
+		this.category = category;
 		this.location = location;
 		this.price = price;
 	}
